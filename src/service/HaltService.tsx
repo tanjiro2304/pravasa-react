86db5b39cc3dfc6edea9351527c5f36fa71e4f16
TheLastNaik <sodaisa_4tsuki@hotmail.com>
2025-03-14 14:50:38 +0530

    Commit changes.

--- src/service/HaltService.tsx
+++ src/service/HaltService.tsx
@@ -1,14 +1,14 @@
-import axios from "axios";
-import RouteFilter from "../models/RouteFilter.tsx";
-
-import HaltDto from "../models/HaltDto.tsx";
-
-export function HaltService() {
-
-    const baseUrl:string = "http://localhost:8011/halt";
-
-    const findHaltByRouteId = async (filter: RouteFilter) => {
-        return await axios.post<HaltDto>(baseUrl + '/findByRouteId', filter);
-    }
-
-}
+// import axios from "axios";
+// import RouteFilter from "../models/RouteFilter.tsx";
+//
+// import HaltDto from "../models/HaltDto.tsx";
+//
+// export function HaltService() {
+//
+//     const baseUrl:string = "http://localhost:8011/halt";
+//
+//     // const findHaltByRouteId = async (filter: RouteFilter) => {
+//     //     return await axios.post<HaltDto>(baseUrl + '/findByRouteId', filter);
+//     // }
+//
+// }
