ee373ad6a66526f970736e125355fb179816483e
TheLastNaik <sodaisa_4tsuki@hotmail.com>
2025-03-14 14:48:56 +0530

    Commit changes.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.jpg", "**/*.png"]
+  assetsInclude: ["**/*.jpg", "**/*.png"],
+  base: "/pravasa-react",
 })
